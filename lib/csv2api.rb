47920f52b4407e9311f1bbc956369dc1bf3f9e07
Jonah Ruiz <jonah@pixelhipsters.com>
2014-06-03 16:41:16 -0400

    Add active_support dependency

--- lib/csv2api.rb
+++ lib/csv2api.rb
@@ -1,6 +1,7 @@
 require 'csv2api/version'
 require 'csv2api/utils'
 require 'csv2api/parser'
+require 'active_support/core_ext'
 
 # @author Jonah Ruiz <jonah@pixelhipsters.com>
 # Auto-creates API endpoints from csv files
