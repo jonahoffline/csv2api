5c96908a579b592a408de6bb9aa1d95000625bb8
Jonah Ruiz <jonah@pixelhipsters.com>
2014-08-21 22:39:12 -0400

    Add active_support before using core_ext to prevent weird travis err

--- lib/csv2api.rb
+++ lib/csv2api.rb
@@ -1,6 +1,7 @@
 require 'csv2api/version'
 require 'csv2api/utils'
 require 'csv2api/parser'
+require 'active_support'
 require 'active_support/core_ext'
 
 # @author Jonah Ruiz <jonah@pixelhipsters.com>
